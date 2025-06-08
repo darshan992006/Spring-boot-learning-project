9e38611a5594cc66b3e617bac9aeac5a64ca1e41
Darshan Pathak <darshanpathak8899@gmail.com>
2025-06-08 11:53:59 +0530

    integrating external APIs

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -2,15 +2,17 @@ package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
-    private static final String apikey="065496db9b4fa4db4cb9e634b2053ab3";
-
+    @Value("${weather_api_key}")
+    private String apikey;
     private static final String API="http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
